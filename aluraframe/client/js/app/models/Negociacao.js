413f69db05221a47da3b99d11be723f32332f0a2
Wesley <wesley.contato@hotmail.com>
2020-07-17 19:28:00 -0300

    Objetos imutáveis

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,9 +1,11 @@
 class Negociacao{
 
     constructor(data, quantidade, valor){
+
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
+        Object.freeze(this);
     }
 
     get volume(){
