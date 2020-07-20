817159026cdfad3ab3d21266fab5179c2e31fdda
Wesley <wesley.contato@hotmail.com>
2020-07-20 06:37:06 -0300

    Resolvendo um problema com o paradigma funcional

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -9,11 +9,14 @@ class NegociacaoController {
 
     }
 
-    adiciona() {
+    adiciona(event) {
 
         event.preventDefault();
 
-        let data = new Date(this._inputData.value.replace(/-/g, ','));
+        let data = new Date(...this._inputData.value.split('-').map(function (item, indice) {
+            return item - indice % 2
+        })
+        );
         console.log(data);
     }
 }
