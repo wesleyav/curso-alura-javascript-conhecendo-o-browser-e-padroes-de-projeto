372f39cc3f1f7595cf2c87d372a4d80888d6918e
Wesley <wesley.contato@hotmail.com>
2020-07-20 06:44:23 -0300

    Arrow Functions: deixando o código ainda menos verboso

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -13,10 +13,18 @@ class NegociacaoController {
 
         event.preventDefault();
 
-        let data = new Date(...this._inputData.value.split('-').map(function (item, indice) {
-            return item - indice % 2
-        })
+        let data = new Date(...
+            this._inputData.value
+            .split('-')
+            .map((item, indice) => item - indice % 2)
+        
         );
-        console.log(data);
+        
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+            );
+        console.log(negociacao);
     }
 }
