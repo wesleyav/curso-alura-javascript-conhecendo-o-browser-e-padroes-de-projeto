3e6be4a7a499dbdc04fa87bc7cb51ae261773a02
Wesley <wesley.contato@hotmail.com>
2020-07-21 07:02:13 -0300

    Criando nosso ListModel

---
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -0,0 +1,19 @@
+class ListaNegociacoes{
+
+    constructor(){
+
+        this._negociacoes = []
+    }
+
+    adiciona(negociacao){
+        
+        this._negociacoes.push(negociacao);
+    }
+
+    get negociacoes(){
+
+        return this._negociacoes;
+
+    }
+
+}
