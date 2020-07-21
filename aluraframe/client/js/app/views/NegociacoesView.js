645dae92bb74cee07f5f4c71e0cec8d5cc72881a
Wesley <wesley.contato@hotmail.com>
2020-07-21 19:26:23 -0300

    Totalizando o volume em nosso Template

--- aluraframe/client/js/app/views/NegociacoesView.js
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -32,6 +32,14 @@ class NegociacoesView{
         </tbody>
         
         <tfoot>
+            <td colspan="3"</td>
+            <td>${
+                (function(){
+                    let total = 0;
+                    model.negociacoes.forEach(n => total+= n.volume);
+                    return total;
+                })()
+            }</td>
         </tfoot>
     </table>
         `;
