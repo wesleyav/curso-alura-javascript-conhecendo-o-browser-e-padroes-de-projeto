97409c7d3fb126aabace911c856931aa6534add6
Wesley <wesley.contato@hotmail.com>
2020-07-22 05:55:03 -0300

    Herança e reutilização de código

--- aluraframe/client/js/app/views/NegociacoesView.js
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,11 +1,10 @@
-class NegociacoesView{
+class NegociacoesView extends View {
 
-    constructor(elemento){
-
-        this._elemento = elemento;
+    constructor(elemento) {
+        super(elemento);
     }
 
-    _template(model){
+    _template(model) {
 
         return `
         <table class="table table-hover table-bordered">
@@ -40,11 +39,4 @@ class NegociacoesView{
     </table>
         `;
     }
-
-    update(model){
-        this._elemento.innerHTML = this._template(model);
-    }
-
-
-
 }
---
+++ aluraframe/client/js/app/views/View.js
@@ -0,0 +1,13 @@
+class View {
+
+    constructor(elemento) {
+
+        this._elemento = elemento;
+    }
+
+    update(model) {
+
+        this._elemento.innerHTML = this._template(model);
+    }
+
+}
